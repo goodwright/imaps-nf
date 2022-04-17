65bb19883b8711b9dcde96601f7f1fd6008e9fb7
Sam Ireland <mail@samireland.com>
2022-04-17 19:30:02 +0100

    Make Samtools Faidx runnable by itself

---
+++ tests/test_faidx_pipeline.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+from unittest import TestCase
+import nextflow
+
+class FastqcRunTests(TestCase):
+
+    def setUp(self):
+        self.pipeline = nextflow.Pipeline(
+            "subworkflows/modules/faidx.nf",
+            config="subworkflows/modules/faidx.config"
+        )
+
+
+    def test_can_run_pipeline(self):
+        execution = self.pipeline.run(params={
+            "fasta": os.path.abspath(Path("assets/Homo_sapiens.smallRNA.fa")),
+        }, profile=["iMaps", "local"])
+        self.assertEqual(execution.status, "OK", msg=execution.stdout)
+        self.assertEqual(len(execution.process_executions), 1)
