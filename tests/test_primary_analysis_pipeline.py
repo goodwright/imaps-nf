ab8200cbd52f4be4a5dabf85505c5692ecf7d576
Sam Ireland <mail@samireland.com>
2022-04-18 21:56:44 +0100

    Primary analysis basic test

---
+++ tests/test_primary_analysis_pipeline.py
@@ -0,0 +1,20 @@
+import os
+from unittest import TestCase
+import nextflow
+
+class PrimaryAnalysisRunTests(TestCase):
+
+    def setUp(self):
+        self.pipeline = nextflow.Pipeline(
+            "subworkflows/primaryanalysis.nf",
+            config="subworkflows/primaryanalysis.config"
+        )
+    
+
+    def test_can_run_pipeline(self):
+        execution = self.pipeline.run(params={
+            "fastq": "assets/ultraplex_demux_iCLIP_SmB_Cal51_NSsiRNA_20130808_LUc21_5.fastq.gz",
+            "Hs_genome": "assets/human_genome",
+        }, profile=["iMaps", "local"])
+        self.assertEqual(execution.status, "OK", msg=execution.stdout)
+        self.assertEqual(len(execution.process_executions), 24)
