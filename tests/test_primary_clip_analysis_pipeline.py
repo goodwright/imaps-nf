139d110a221e5ef04c56d666ffa465c62bdfc934
Marc Jones <marc.jones@crick.ac.uk>
2022-06-09 14:59:05 +0100

    Update tests

--- tests/test_primary_clip_analysis_pipeline.py
+++ tests/test_primary_clip_analysis_pipeline.py
@@ -18,7 +18,7 @@ class PrimaryClipAnalysisRunTests(TestCase):
             "Hs_genome": "assets/human_genome",
         }, profile=["iMaps", "local", "test"])
         self.assertEqual(execution.status, "OK", msg=execution.stdout)
-        self.assertEqual(len(execution.process_executions), 29)
+        self.assertEqual(len(execution.process_executions), 30)
 
         # Default UMI Separator is rbc
         for proc in execution.process_executions:
@@ -48,7 +48,7 @@ class PrimaryClipAnalysisRunTests(TestCase):
                 "Hs_genome": "assets/human_genome_no_gzip",
             }, profile=["iMaps", "local", "test"])
             self.assertEqual(execution.status, "OK", msg=execution.stdout)
-            self.assertEqual(len(execution.process_executions), 29)
+            self.assertEqual(len(execution.process_executions), 30)
         finally:
             if os.path.exists("assets/human_genome_no_gzip"):
                 shutil.rmtree("assets/human_genome_no_gzip")
