c45ad18ba70e322253813e00526c00b5c04a1678
Sam Ireland <mail@samireland.com>
2022-06-10 13:27:18 +0100

    Add fastqc_single_end param to demultiplex

--- tests/test_demultiplex_pipeline.py
+++ tests/test_demultiplex_pipeline.py
@@ -52,3 +52,45 @@ class DemultiplexRunTests(PipelineTest):
         ])
         self.check_results("csv_to_barcode", ["barcode.csv"])
         self.check_results("xlsx_to_csv", ["TEST_ANNOTATION.xlsx.csv"])
+    
+
+    def test_can_run_pipeline_single_end_fastqc(self):
+        execution = self.pipeline.run(params={
+            "annotation": os.path.abspath(Path("assets/TEST_ANNOTATION.csv")),
+            "multiplexed_fastq": os.path.abspath(Path("assets/SmB_multiplexed.fq.gz")),
+            "fastqc_single_end": "true"
+        }, profile=["iMaps", "local"], location="testlocation")
+        self.check_execution_ok(execution, 4)
+        self.check_results("fastqc", [
+            "iCLIP_SmB_Cal51_NSsiRNA_20130808_LUc21_4_fastqc.zip",
+            "iCLIP_SmB_Cal51_NSsiRNA_20130808_LUc21_4_fastqc.html",
+            "iCLIP_SmB_Cal51_NSsiRNA_20130808_LUc21_5_fastqc.zip",
+            "iCLIP_SmB_Cal51_NSsiRNA_20130808_LUc21_5_fastqc.html"
+        ])
+        self.check_results("ultraplex", [
+            "ultraplex_demux_iCLIP_SmB_Cal51_NSsiRNA_20130808_LUc21_5.fastq.gz",
+            "ultraplex_demux_iCLIP_SmB_Cal51_NSsiRNA_20130808_LUc21_4.fastq.gz",
+            r"ultraplex_\d+\.\d+\.log"
+        ])
+        self.check_results("csv_to_barcode", ["barcode.csv"])
+    
+
+    def test_can_run_pipeline_paired_end_fastqc(self):
+        execution = self.pipeline.run(params={
+            "annotation": os.path.abspath(Path("assets/TEST_ANNOTATION.csv")),
+            "multiplexed_fastq": os.path.abspath(Path("assets/SmB_multiplexed.fq.gz")),
+            "fastqc_single_end": "false"
+        }, profile=["iMaps", "local"], location="testlocation")
+        self.check_execution_ok(execution, 4)
+        self.check_results("fastqc", [
+            "iCLIP_SmB_Cal51_NSsiRNA_20130808_LUc21_4_1_fastqc.zip",
+            "iCLIP_SmB_Cal51_NSsiRNA_20130808_LUc21_4_1_fastqc.html",
+            "iCLIP_SmB_Cal51_NSsiRNA_20130808_LUc21_5_1_fastqc.zip",
+            "iCLIP_SmB_Cal51_NSsiRNA_20130808_LUc21_5_1_fastqc.html"
+        ])
+        self.check_results("ultraplex", [
+            "ultraplex_demux_iCLIP_SmB_Cal51_NSsiRNA_20130808_LUc21_5.fastq.gz",
+            "ultraplex_demux_iCLIP_SmB_Cal51_NSsiRNA_20130808_LUc21_4.fastq.gz",
+            r"ultraplex_\d+\.\d+\.log"
+        ])
+        self.check_results("csv_to_barcode", ["barcode.csv"])
